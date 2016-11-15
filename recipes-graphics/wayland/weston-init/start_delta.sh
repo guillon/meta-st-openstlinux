245b966963b8941a059deaf2e5bcf1dc4c6f31e9
Romuald JEANNE <romuald.jeanne@st.com>
2016-11-15 16:14:49 +0100

    WESTON: TMP workaround to start DELTA video firmware (update)
    Test first if delta is running or not before trying to start it.
    
    Change-Id: I5d7b124d828036ad7463a9d7149efbbec4883ca0
    Signed-off-by: Romuald JEANNE <romuald.jeanne@st.com>
    Reviewed-on: https://gerrit.st.com/60414

--- recipes-graphics/wayland/weston-init/start_delta.sh
+++ recipes-graphics/wayland/weston-init/start_delta.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-echo stop > /sys/class/remoteproc/remoteproc0/state
-echo start > /sys/class/remoteproc/remoteproc0/state
+if [ -e /sys/class/remoteproc/remoteproc0/state ]; then
+    if [ -z $(grep running /sys/class/remoteproc/remoteproc0/state)]; then
+        echo start > /sys/class/remoteproc/remoteproc0/state
+    fi
+else
+    echo start > /sys/class/remoteproc/remoteproc0/state
+fi
