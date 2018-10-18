bb84ac3f62f75f7a617fd758210f01909bb6e65b
Christophe Priouzeau <christophe.priouzeau@st.com>
2018-10-18 17:27:52 +0200

    USB-OTG: disable mass-storage
    Due to filesystem corruption, mass-storage are
    disabled.
    
    Change-Id: Icf729629d44e4cd01b664150ac409fef512994a9
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@st.com>
    Reviewed-on: https://gerrit.st.com/109067
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Christophe MONTAUD <christophe.montaud@st.com>

--- recipes-bsp/tools/usbotg-msc-eth-config/stm32_usbotg_msc_eth_config.sh
+++ recipes-bsp/tools/usbotg-msc-eth-config/stm32_usbotg_msc_eth_config.sh
@@ -20,111 +20,110 @@ IP="192.168.7.2"
 NETMASK="255.255.255.0"
 
 do_start() {
-  if [ ! -d ${configfs} ]; then
-    modprobe libcomposite
     if [ ! -d ${configfs} ]; then
-      exit 1
+        modprobe libcomposite
+        if [ ! -d ${configfs} ]; then
+        exit 1
+        fi
     fi
-  fi
 
-  if [ -d ${d} ]; then
-    exit 0
-  fi
+    if [ -d ${d} ]; then
+        exit 0
+    fi
 
-  udc=$(ls -1 /sys/class/udc/)
-	if [ -z $udc ]; then
-    echo "No UDC driver registered"
-    exit 1
-  fi
+    udc=$(ls -1 /sys/class/udc/)
+    if [ -z $udc ]; then
+        echo "No UDC driver registered"
+        exit 1
+    fi
 
-  mkdir "${d}"
-  echo ${VENDOR_ID} > "${d}/idVendor"
-  echo ${PRODUCT_ID} > "${d}/idProduct"
-  echo 0x0200 > "${d}/bcdUSB"
-  echo "0xEF" > "${d}/bDeviceClass"
-  echo "0x02" > "${d}/bDeviceSubClass"
-  echo "0x01" > "${d}/bDeviceProtocol"
-  echo "0x0100" > "${d}/bcdDevice"
+    mkdir "${d}"
+    echo ${VENDOR_ID} > "${d}/idVendor"
+    echo ${PRODUCT_ID} > "${d}/idProduct"
+    echo 0x0200 > "${d}/bcdUSB"
+    echo "0xEF" > "${d}/bDeviceClass"
+    echo "0x02" > "${d}/bDeviceSubClass"
+    echo "0x01" > "${d}/bDeviceProtocol"
+    echo "0x0100" > "${d}/bcdDevice"
 
-  mkdir -p "${d}/strings/0x409"
-  echo "0" > "${d}/strings/0x409/serialnumber"
-  echo "STMicroelectronics" > "${d}/strings/0x409/manufacturer"
-  echo "STM32MP1" > "${d}/strings/0x409/product"
+    mkdir -p "${d}/strings/0x409"
+    echo "0" > "${d}/strings/0x409/serialnumber"
+    echo "STMicroelectronics" > "${d}/strings/0x409/manufacturer"
+    echo "STM32MP1" > "${d}/strings/0x409/product"
 
-  # Config
-  mkdir -p "${d}/configs/${c}"
-  mkdir -p "${d}/configs/${c}/strings/0x409"
-  echo "Config 1: RNDIS" > "${d}/configs/${c}/strings/0x409/configuration"
-  echo 250 > "${d}/configs/${c}/MaxPower"
-  echo 0xC0 > "${d}/configs/${c}/bmAttributes" # self powered device
+    # Config
+    mkdir -p "${d}/configs/${c}"
+    mkdir -p "${d}/configs/${c}/strings/0x409"
+    echo "Config 1: RNDIS" > "${d}/configs/${c}/strings/0x409/configuration"
+    echo 250 > "${d}/configs/${c}/MaxPower"
+    echo 0xC0 > "${d}/configs/${c}/bmAttributes" # self powered device
 
-  mkdir -p "${d}/functions/${func_eth}"
-  mkdir -p "${d}/functions/${func_ms}"
+#    mkdir -p "${d}/functions/${func_ms}"
+#    # Mass-Storage function
+#    echo 0 > "${d}/functions/${func_ms}/stall"
+#    echo 0 > "${d}/functions/${func_ms}/lun.0/cdrom"
+#    echo 0 > "${d}/functions/${func_ms}/lun.0/ro"
+#    echo 0 > "${d}/functions/${func_ms}/lun.0/nofua"
+#    echo 1 > "${d}/functions/${func_ms}/lun.0/removable"
+#    if [ -z "$1" ]; then
+#        echo "Using /dev/mmcblk0"
+#        echo "/dev/mmcblk0" > "${d}/functions/${func_ms}/lun.0/file"
+#    else
+#        echo "Using $1"
+#        echo "$1" > "${d}/functions/${func_ms}/lun.0/file"
+#    fi
 
-  # Mass-Storage function
-  echo 0 > "${d}/functions/${func_ms}/stall"
-  echo 0 > "${d}/functions/${func_ms}/lun.0/cdrom"
-  echo 0 > "${d}/functions/${func_ms}/lun.0/ro"
-  echo 0 > "${d}/functions/${func_ms}/lun.0/nofua"
-  echo 1 > "${d}/functions/${func_ms}/lun.0/removable"
-  if [ -z "$1" ]; then
-	  echo "Using /dev/mmcblk0"
-	  echo "/dev/mmcblk0" > "${d}/functions/${func_ms}/lun.0/file"
-	else
-	  echo "Using $1"
-	  echo "$1" > "${d}/functions/${func_ms}/lun.0/file"
-	fi
+    mkdir -p "${d}/functions/${func_eth}"
+    # Windows extension to force RNDIS config
+    mkdir -p "${d}/os_desc"
+    echo "1" > "${d}/os_desc/use"
+    echo "0xbc" > "${d}/os_desc/b_vendor_code"
+    echo "MSFT100" > "${d}/os_desc/qw_sign"
 
-	# Windows extension to force RNDIS config
-  mkdir -p "${d}/os_desc"
-  echo "1" > "${d}/os_desc/use"
-  echo "0xbc" > "${d}/os_desc/b_vendor_code"
-  echo "MSFT100" > "${d}/os_desc/qw_sign"
+    mkdir -p "${d}/functions/${func_eth}/os_desc/interface.rndis"
+    echo "RNDIS" > "${d}/functions/${func_eth}/os_desc/interface.rndis/compatible_id"
+    echo "5162001" > "${d}/functions/${func_eth}/os_desc/interface.rndis/sub_compatible_id"
 
-  mkdir -p "${d}/functions/${func_eth}/os_desc/interface.rndis"
-  echo "RNDIS" > "${d}/functions/${func_eth}/os_desc/interface.rndis/compatible_id"
-  echo "5162001" > "${d}/functions/${func_eth}/os_desc/interface.rndis/sub_compatible_id"
+    # Set up the rndis device only first
+    ln -s "${d}/functions/${func_eth}" "${d}/configs/${c}"
+#    ln -s "${d}/functions/${func_ms}" "${d}/configs/${c}"
+    ln -s "${d}/configs/${c}" "${d}/os_desc"
 
-  # Set up the rndis device only first
-  ln -s "${d}/functions/${func_eth}" "${d}/configs/${c}"
-  ln -s "${d}/functions/${func_ms}" "${d}/configs/${c}"
-  ln -s "${d}/configs/${c}" "${d}/os_desc"
+    echo "${udc}" > "${d}/UDC"
 
-  echo "${udc}" > "${d}/UDC"
+    sleep 0.2
 
-  sleep 0.2
-
-  ifconfig usb0 $IP $NETMASK
-  ifconfig usb0 up
+    ifconfig usb0 $IP $NETMASK
+    ifconfig usb0 up
 }
 
 do_stop() {
-  ifconfig usb0 down
+    ifconfig usb0 down
 
-  sleep 0.2
+    sleep 0.2
 
-  echo "" > "${d}/UDC"
+    echo "" > "${d}/UDC"
 
-	rm -f "${d}/os_desc/${c}"
-  rm -f "${d}/configs/${c}/${func_eth}"
-  rm -f "${d}/configs/${c}/${func_ms}"
+    rm -f "${d}/os_desc/${c}"
+    [ -d "${d}/configs/${c}/${func_eth}" ] &&rm -f "${d}/configs/${c}/${func_eth}"
+    [ -d "${d}/configs/${c}/${func_ms}" ] && rm -f "${d}/configs/${c}/${func_ms}"
 
-  [ -d "${d}/strings/0x409/" ] && rmdir "${d}/strings/0x409/"
-  [ -d "${d}/configs/${c}/strings/0x409" ] && rmdir "${d}/configs/${c}/strings/0x409"
-  [ -d "${d}/configs/${c}" ] && rmdir "${d}/configs/${c}"
-  [ -d "${d}/functions/${func_ms}" ] && rmdir "${d}/functions/${func_ms}"
-  [ -d "${d}/functions/${func_eth}" ] && rmdir "${d}/functions/${func_eth}"
-  [ -d "${d}" ] && rmdir "${d}"
+    [ -d "${d}/strings/0x409/" ] && rmdir "${d}/strings/0x409/"
+    [ -d "${d}/configs/${c}/strings/0x409" ] && rmdir "${d}/configs/${c}/strings/0x409"
+    [ -d "${d}/configs/${c}" ] && rmdir "${d}/configs/${c}"
+    [ -d "${d}/functions/${func_ms}" ] && rmdir "${d}/functions/${func_ms}"
+    [ -d "${d}/functions/${func_eth}" ] && rmdir "${d}/functions/${func_eth}"
+    [ -d "${d}" ] && rmdir "${d}"
 }
 
 case $1 in
-	start)
-		do_start $2
-		;;
-	stop)
-		do_stop
-		;;
-	*)
-		echo "Usage: $0 (stop | start) [/dev/xxx (if not specified /dev/mmcblk0 will be used by default)]"
-		;;
+    start)
+        do_start $2
+        ;;
+    stop)
+        do_stop
+        ;;
+    *)
+        echo "Usage: $0 (stop | start) [/dev/xxx (if not specified /dev/mmcblk0 will be used by default)]"
+        ;;
 esac
