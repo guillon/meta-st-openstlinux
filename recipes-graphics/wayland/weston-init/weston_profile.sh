649c1d5c7838e130d98cac2571d728187d8329e5
Christophe Priouzeau <christophe.priouzeau@st.com>
2020-02-28 11:12:13 +0100

    WESTON-INIT: remove qt config on shell profile
    The QT configuration enter on conflict with others potential qt
    configuration
    
    Change-Id: Ibd4901a22022a749da9f5f893f62756a94bf12f2
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@st.com>
    Reviewed-on: https://gerrit.st.com/c/oeivi/oe/st/meta-opensdk/+/159492
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>

--- recipes-graphics/wayland/weston-init/weston_profile.sh
+++ recipes-graphics/wayland/weston-init/weston_profile.sh
@@ -1,6 +1,5 @@
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
-export QT_QPA_PLATFORM=wayland
 export ELM_ENGINE=wayland_shm
 export ECORE_EVAS_ENGINE=wayland_shm
 export ECORE_EVAS_ENGINE=wayland_shm
