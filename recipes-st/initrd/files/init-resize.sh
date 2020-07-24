6e349ae1ab5d7c840bae6f79171a2cb3b18ef676
Christophe Priouzeau <christophe.priouzeau@st.com>
2020-07-24 10:45:37 +0200

    INIT-RESIZE: correct case of parition which doesn't contains fs
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@st.com>
    Change-Id: I3e407c7ea103cd1bbbecad87500d7494c2d382d2
    Reviewed-on: https://gerrit.st.com/c/oeivi/oe/st/meta-opensdk/+/173543
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Romuald JEANNE <romuald.jeanne@st.com>

--- recipes-st/initrd/files/init-resize.sh
+++ recipes-st/initrd/files/init-resize.sh
@@ -5,11 +5,6 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin
 ln -s /proc/mounts /etc/mtab
 
 if [ -n "$ROOTFS_DIR" ]; then
-USERFS=
-ROOTFS=
-VENDORFS=
-BOOTFS=
-
     if [ ! -e $ROOTFS_DIR/etc/.resized ]
     then
         for j in 0 1;
@@ -18,30 +13,30 @@ BOOTFS=
             do
                 DEVICE="/dev/mmcblk"$j"p"$i
                 if [ -e $DEVICE ]; then
-                    label=$(/sbin/e2label $DEVICE)
-                    case $label in
-                    user*)
-                        USERFS=$DEVICE
-                        echo "RESIZE USERFS [$USERFS]"
-                        /sbin/e2fsck -f -y -c -C 0 $USERFS && /sbin/resize2fs $USERFS
-                        ;;
-                    root*)
-                        ROOTFS=$DEVICE
-                        echo "RESIZE ROOTFS [$ROOTFS]"
-                        /sbin/resize2fs $ROOTFS
-                        ;;
-                    vendor*)
-                        VENDORFS=$DEVICE
-                        echo "RESIZE VENDORFS [$VENDORFS]"
-                        /sbin/e2fsck -f -y -c -C 0 $VENDORFS && /sbin/resize2fs $VENDORFS
-                        ;;
-                    boot*)
-                        BOOTFS=$DEVICE
-                        echo "RESIZE BOOTFS [$BOOTFS]"
-                        /sbin/e2fsck -f -y -c -C 0 $BOOTFS && /sbin/resize2fs $BOOTFS
-                        ;;
-                     esac
-                 fi
+                    label=$(/sbin/e2label $DEVICE 2> /dev/null)
+                    if [ $? -eq 0 ]; then
+                        case $label in
+                        user*)
+                            echo "RESIZE USERFS [$DEVICE]"
+                            /sbin/e2fsck -f -y -c -C 0 $DEVICE && /sbin/resize2fs $DEVICE
+                            ;;
+                        root*)
+                            echo "RESIZE ROOTFS [$DEVICE]"
+                            /sbin/resize2fs $DEVICE
+                            ;;
+                        vendor*)
+                            echo "RESIZE VENDORFS [$DEVICE]"
+                            /sbin/e2fsck -f -y -c -C 0 $DEVICE && /sbin/resize2fs $DEVICE
+                            ;;
+                        boot*)
+                            echo "RESIZE BOOTFS [$DEVICE]"
+                            /sbin/e2fsck -f -y -c -C 0 $DEVICE && /sbin/resize2fs $DEVICE
+                            ;;
+                        *)
+                            ;;
+                        esac
+                    fi
+                fi
             done
         done
 
